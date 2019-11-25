4a0074df9ae92f35c57d5f85910347eb8ba914e3
suhaylx <akmsodikov@gmail.com>
2019-11-25 05:41:02 +0000

    using string methods

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ print(selfish[::2])
 print(selfish[1:9:2]) # we go from " " and then skip by 2 and next is m and next is s and l
 print(selfish[::-1]) # reverse 
 print(selfish[-2]) # it will print h in as its reverse order
+
+
+quote = 'to be or not to be '
+print(quote.upper())
+print(quote.capitalize())
+print(quote.isprintable())
+print(quote.replace("be", "me")) 
+# Never for get strings are inmutable
