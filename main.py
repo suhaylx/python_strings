df0e8687fb85e0aad512ba35fab6c12ad452c1b0
suhaylx <akmsodikov@gmail.com>
2019-11-25 05:27:42 +0000

    strings and slicing

---
+++ main.py
@@ -0,0 +1,11 @@
+selfish = "I am selfish "
+print (selfish[7])
+
+print('\n')
+print(selfish[:5]) #it means start from begining of string and stop at 5 char
+
+print(selfish[::2])
+
+print(selfish[1:9:2]) # we go from " " and then skip by 2 and next is m and next is s and l
+print(selfish[::-1]) # reverse 
+print(selfish[-2]) # it will print h in as its reverse order
